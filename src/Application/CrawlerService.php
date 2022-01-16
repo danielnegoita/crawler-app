5d4273caa455e25c220f011345a9002c757b7488
Daniel <hello@danielnegoita.com>
2022-01-16 16:13:40 +0200

    feat: save html to file given a url

--- src/Application/CrawlerService.php
+++ src/Application/CrawlerService.php
@@ -3,21 +3,28 @@
 namespace App\Application;
 
 use App\Domain\Url;
+use App\Domain\File;
 use App\Domain\Model\Link;
 use App\Domain\CrawlerInterface;
+use App\Domain\FileSystemRepositoryInterface;
 use App\Domain\Model\LinkRepositoryInterface;
 
 class CrawlerService
 {
     private CrawlerInterface $crawler;
+
     private LinkRepositoryInterface $linkRepository;
+
+    private FileSystemRepositoryInterface $fileSystemRepository;
 
     public function __construct(
         CrawlerInterface $crawler,
-        LinkRepositoryInterface $linkRepository
+        LinkRepositoryInterface $linkRepository,
+        FileSystemRepositoryInterface $fileSystemRepository
     ) {
         $this->crawler = $crawler;
         $this->linkRepository = $linkRepository;
+        $this->fileSystemRepository = $fileSystemRepository;
     }
 
     public function saveLinksFromPage(Url $url): self
@@ -31,6 +38,19 @@ class CrawlerService
         }
 
         $this->linkRepository->save();
+
+        return $this;
+    }
+
+    public function savePageHtmlToFile(Url $url)
+    {
+        $filename = $url->toEncode() . '.html';
+
+        $html = $this->crawler->extractHtmlFromPage($url);
+
+        $file = new File($filename, $html, 'files'); // TODO: move file location value to .env
+
+        $this->fileSystemRepository->saveFile($file);
 
         return $this;
     }
