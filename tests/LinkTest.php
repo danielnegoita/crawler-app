c6fa8c2b595efc89f6d2489fc810f2d67036d96f
Daniel <hello@danielnegoita.com>
2022-01-16 11:46:39 +0200

    add tests for link class

---
+++ tests/LinkTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests;
+
+use App\Domain\Url;
+use App\Domain\Model\Link;
+use PHPUnit\Framework\TestCase;
+
+class LinkTest extends TestCase
+{
+    public function testCanCreateLink()
+    {
+        $page = 'http://example.com';
+        $pageLink = 'http://test.com';
+
+        $link = Link::create(Url::fromString($pageLink), $page);
+
+        $this->assertEquals($page, $link->page());
+        $this->assertEquals($pageLink, $link->link()->toString());
+    }
+
+    public function testCanReturnLinkAsArray()
+    {
+        $linkArray = [
+            'page' => 'http://example.com',
+            'link' => 'http://test.com'
+        ];
+
+        $link = Link::create(
+            Url::fromString($linkArray['link']),
+            $linkArray['page']
+        );
+
+        $this->assertEquals($linkArray, $link->toArray());
+    }
+}
