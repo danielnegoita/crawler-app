1e6de7d3950107ff9c9a560140d3a5b41b21c1d8
Daniel <hello@danielnegoita.com>
2022-01-20 10:12:20 +0200

    setup micro-kernel in app module

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+
+$kernel = new Kernel('dev', true);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
