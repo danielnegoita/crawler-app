fe011fe468da994334b349598ee68fa4ccb8c7ce
Daniel <hello@danielnegoita.com>
2022-01-21 11:36:28 +0200

    show go to admin button when user is authenticated

--- src/App/Controller/HomeController.php
+++ src/App/Controller/HomeController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use App\Services\AuthService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends Controller
 {
+    private AuthService $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
     /**
      * @Route("/")
      */
     public function index(): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('home.html.twig', [
+            'isAuthenticated' => null !== $this->authService->getCredentials()
+        ]);
     }
 }
