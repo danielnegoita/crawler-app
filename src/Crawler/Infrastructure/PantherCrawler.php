bb7e8ca75288bbe5b2576b9f64fa784d408370ae
Daniel <hello@danielnegoita.com>
2022-01-25 14:29:15 +0200

    refactor: remove redundant global var

--- src/Crawler/Infrastructure/PantherCrawler.php
+++ src/Crawler/Infrastructure/PantherCrawler.php
@@ -61,7 +61,7 @@ final class PantherCrawler implements CrawlerInterface
 
     private function crawl(string $url): Crawler
     {
-        $this->client = Client::createChromeClient(PROJECT_DIR . $_ENV['CHROME_DRIVER_PATH']);
+        $this->client = Client::createChromeClient(dirname(__DIR__, 3) . '/drivers/chromedriver');
 
         $this->client->request('GET', $url);
 
