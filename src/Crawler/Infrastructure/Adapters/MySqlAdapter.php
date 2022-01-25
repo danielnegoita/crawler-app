59e0af10db898a50c521b7720313536b46af460f
Daniel <hello@danielnegoita.com>
2022-01-25 18:12:31 +0200

    feat: save crawled page links to db

--- src/Crawler/Infrastructure/Adapters/MySqlAdapter.php
+++ src/Crawler/Infrastructure/Adapters/MySqlAdapter.php
@@ -2,13 +2,34 @@
 
 namespace Crawler\Infrastructure\Adapters;
 
+use PDO;
 use Crawler\Infrastructure\StorageAdaptorInterface;
 
 class MySqlAdapter implements StorageAdaptorInterface
 {
+    private PDO $connection;
+
+    public function __construct()
+    {
+        $this->connection = new PDO(
+            'mysql:host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_DATABASE'],
+            $_ENV['DB_USER'],
+            $_ENV['DB_PASSWORD'],
+            [ PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ ]
+        );
+    }
+
     public function saveLinks(array $links): bool
     {
-        // TODO: Implement saveLinks() method.
-        return true;
+        $query = 'INSERT INTO links (link, page)
+                  VALUES (?, ?)';
+
+        $stmt = $this->connection->prepare($query);
+
+        foreach ($links as $link) {
+            $stmt->execute([ $link['link'], $link['page'] ]);
+        }
+
+        return !!$this->connection->lastInsertId();
     }
 }
